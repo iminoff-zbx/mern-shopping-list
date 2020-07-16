5c2370c23f0dee9512c3523bf9fa897d9c65cd9b
navruz-zbx <iminov.zbx@gmail.com>
2020-07-16 21:52:01 +0500

    MongoDB connected

--- config/configuration.js
+++ config/configuration.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/shopping_list',
+    mongoURI: 'mongodb+srv://navruz-zbx:11235813zbx@cluster0.mogpg.mongodb.net/<dbname>?retryWrites=true&w=majority',
     PORT: process.env.PORT || 5000
 }
+
+// mongoURI: 'mongodb://localhost:27017/shopping_list',
