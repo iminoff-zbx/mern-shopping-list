bbb082aeecd17e92f16bdd4fe2b9309609bc8bbf
navruz-zbx <iminov.zbx@gmail.com>
2020-07-16 20:14:13 +0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -24,15 +24,14 @@ mongoose
 app.use('/api/items', items);
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    
-    // Set Static folder
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
-    })
-}
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
 
