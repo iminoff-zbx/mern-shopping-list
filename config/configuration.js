e277a865859070d7dcd0421f0d8f1826258b7e4b
navruz-zbx <iminov.zbx@gmail.com>
2020-07-16 23:24:03 +0500

    make it better

--- config/configuration.js
+++ config/configuration.js
@@ -2,5 +2,3 @@ module.exports = {
     mongoURI: 'mongodb+srv://navruz-zbx:11235813zbx@cluster0.mogpg.mongodb.net/<dbname>?retryWrites=true&w=majority',
     PORT: process.env.PORT || 5000
 }
-
-// mongoURI: 'mongodb://localhost:27017/shopping_list',
