c248abbb58d187efa31512315fbb7da15dd992d4
navruz-zbx <iminov.zbx@gmail.com>
2020-07-16 17:51:51 +0500

    Connect to BackEnd

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,8 +11,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import { v4 as uuidv4 } from 'uuid';
-
 
 class ItemModal extends Component {
 
@@ -35,7 +33,6 @@ class ItemModal extends Component {
         e.preventDefault();
 
         const newItem = {
-            id: uuidv4(),
             name: this.state.name
         }
 
