f8a76c3e84a71938faeb2246686a48dd5469a6c0
navruz-zbx <iminov.zbx@gmail.com>
2020-07-16 19:21:23 +0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/itemsRoute');
 
@@ -21,6 +22,17 @@ mongoose
 
 // Use Routes
 app.use('/api/items', items);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    
+    // Set Static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
 
