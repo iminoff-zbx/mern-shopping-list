9de027ea600a4a250270e0ac31bfd7712b3c3a35
navruz-zbx <iminov.zbx@gmail.com>
2020-07-17 16:27:59 +0500

    db name added

--- config/configuration.js
+++ config/configuration.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://navruz-zbx:11235813zbx@cluster0.mogpg.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://navruz-zbx:11235813zbx@cluster0.mogpg.mongodb.net/shopping-list?retryWrites=true&w=majority',
     PORT: process.env.PORT || 5000
 }
